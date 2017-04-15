5839ea9cb19bd2cecede9758991dfbf0d21c177f
Ankesh Samarth <aankeshsamarth@gmail.com>
2017-04-16 02:06:12 +0530

    Restructured and developed API

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,8 +1,4 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :body
   belongs_to :user
-
-  class UserSerializer < ActiveModel::Serializer
-    attributes :id, :name
-  end
 end
